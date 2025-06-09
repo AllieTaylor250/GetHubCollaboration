ad2c0514746c1d416a30cba6aae0bcbd87fe343d
Allie <atay2010@icloud.com>
2025-06-09 09:21:48 -0500

    Initial Commit

---
+++ GetHubCollaboration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetHubCollaboration
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetHubCollaboration/GetHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetHubCollaborationApp.swift
+//  GetHubCollaboration
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GetHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
